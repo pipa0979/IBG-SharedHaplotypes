820c770cfba699f0829d97f2257fb1ee96c488f0
pipa0979 <piyush.patel@colorado.edu>
2016-03-15 02:10:08 -0600

    Added the in program comression version using zlib. Both versions of the program are stored in respective folders

---
+++ IBG-SharedHaplotype_in_programCompression/ReadMe.txt
@@ -0,0 +1,22 @@
+This is the version 2 of the IBG-SharedHaplotype program. With in-program compression.
+
+There are 2 flags (which are discussed in the readme file):
+
+Flags used:
+--in <filename.int>
+--out <somefilename.log>
+
+There's a makefile as well. The log file with be formed automatically with the name <inputfilename>.log
+
+
+To compile:
+make all
+
+To run:
+./SharedHaplotype --in out1.pop1.gen20.chr10.int --out testoutput
+
+
+
+Size file before compression:	274.6 MB
+Size file before compression:	31.7  MB
+Compression Ratio:	8.662 
---
+++ IBG-SharedHaplotype_in_programCompression/SharedHaplotypeMain.cpp
@@ -0,0 +1,95 @@
+//============================================================================
+// Name        : SharedHaplotypeMatching.cpp
+// Author      : Piyush
+// Version     :
+// Copyright   :
+// Description : Main Program. Entry gate.
+//============================================================================
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <fstream>
+#include "ReadFiles.hpp"
+#include "Part.hpp"
+
+int main(int argc,char *argv[]) {
+clock_t begin = clock();
+
+ReadFiles inpfile;
+std::string INPFILENAME="";
+std::string OUTPUTFILENAME="";
+std::string LOGFILENAME="";
+
+
+for (int i=0;i<argc;i++)
+{
+	if(strcmp(argv[i],"--in")==0)
+	{
+		INPFILENAME=argv[++i];
+		//LOGFILENAME=INPFILENAME;
+	}
+	else if (strcmp(argv[i],"--out")==0)
+	{
+		OUTPUTFILENAME=argv[++i];
+	}
+
+}
+
+/*ERROR CHECKING*/
+if(INPFILENAME!="")
+{
+	if (strcmp(INPFILENAME.substr(INPFILENAME.find_last_of(".")).c_str(),".int")!=0)
+		{
+			std::cerr<<"--in <filename> not provided"<<std::endl;
+			exit(0);
+		}
+}
+if (OUTPUTFILENAME=="")
+	{
+		std::cerr<<"--out <filename> not provided"<<std::endl;
+		exit(0);
+	}
+
+/*
+if (LOGFILENAME == "")
+{
+	std::cerr<<"--log <filename> not provided"<<std::endl;
+	exit(0);
+}
+*/
+/*ERROR CHECKING*/
+
+
+
+
+//std::cout<<"INPFILENAME = "<<INPFILENAME <<std::endl;
+if(!inpfile.readFile(INPFILENAME,OUTPUTFILENAME))
+{
+	std::cerr<<"Couldn't open file "<<INPFILENAME<<" program is exiting"<<std::endl;
+	exit(0);
+}
+part ind1(inpfile);
+part ind2(inpfile);
+part compute;
+//compute.quickSort_hapindex(ind1);
+compute.computation_haplotype(ind1,ind2);
+
+
+
+LOGFILENAME= INPFILENAME.substr(0,INPFILENAME.find(".int"));
+std::ofstream writeLogFile((LOGFILENAME+".log").c_str());	//Create the NON-compressed file
+
+
+clock_t end = clock();
+double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+
+if (!writeLogFile.is_open())
+	{
+		std::cerr<<LOGFILENAME<<" cannot be created.. Program exiting"<<std::endl;
+		exit(0);
+	}
+writeLogFile<<" The elapsed time is "<<elapsed_secs <<" secs."<<std::endl;
+writeLogFile.close();
+
+return 0;
+}
